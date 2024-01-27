dc7e727562e8a5fd7e57188562d756048ef61f20
monkriz1991 <70541488+monkriz1991@users.noreply.github.com>
2024-01-27 23:19:17 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,26 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "cryptoscool",
+//       exec_mode: "cluster",
+//       instances: "max", //
+//       script: "./node_modules/nuxt/bin/nuxt.mjs",
+//       args: "start",
+//       port: 8880,
+//     },
+//   ],
+// };
 module.exports = {
-  apps: [
+  apps : [
+    // {
+    //   name: "nuxt-dev",
+    //   script: "npm",
+    //   args: "run dev"
+    // },
     {
-      name: "cryptoscool",
-      exec_mode: "cluster",
-      instances: "max", //
-      script: "./node_modules/nuxt/bin/nuxt.mjs",
-      args: "start",
-      port: 8880,
-    },
-  ],
-};
+      name: "nuxt-prod",
+      script: "npm",
+      args: "run start"
+    }
+  ]
+}
