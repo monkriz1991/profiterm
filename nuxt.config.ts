cb588175a66f0d0d38d307316ce3b2a209d0294d
krizis <monkriz1991@gmail.com>
2024-01-31 15:25:41 +0300

    er

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,10 +56,6 @@ export default defineNuxtConfig({
   ],
   nitro: {
     plugins: ["~/server/index.ts"],
-    output: {
-      dir: ".output",
-      publicDir: ".output/public",
-    },
   },
   auth: {
     baseURL: process.env.AUTH_ORIGIN,
--- server/api/multiupload/index.post.js
+++ server/api/multiupload/index.post.js
@@ -7,7 +7,7 @@ export default defineEventHandler(async (event) => {
     let fileNames = [];
     const storage = multer.diskStorage({
       destination: (req, file, cb) => {
-        cb(null, ".output/public/assets/images");
+        cb(null, "public/assets/images");
       },
       filename: (req, file, cbd) => {
         const filePath = file.originalname;
