fc80b8b7c3ed094a1fc522589f05db1f92eee523
mynmind <mynmind@proton.me>
2024-07-06 12:58:57 +0300

    env

--- server/api/contact/index.post.js
+++ server/api/contact/index.post.js
@@ -4,9 +4,13 @@ const config = useRuntimeConfig();
 const transporter = nodemailer.createTransport({
   host: config.MAILHOST,
   port: config.MAILPORT,
+  secure: false,
   auth: {
     user: config.MAILUSER,
     pass: config.MAILPASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
@@ -14,33 +18,35 @@ export default defineEventHandler(async (event, response) => {
   try {
     const body = await readBody(event);
     const mail = {
-      form: `"${body.name}" <${body.email}>`,
+      from: `"Заявка с сайта" <${config.MAILUSER}>`,
       to: config.CONTACTMAIL,
       subject: "Заявка с сайта Профитерм",
       text: body.info,
       html: `
-      / Имя / ${body.name} <br>
-      / Телефон / ${body.phone} <br>
-      / Email / ${body.email} <br>
-      / Адрес объекта / ${body.info} <br>
-      / Комментарий / ${body.description} <br>
-    `,
+        / Имя / ${body.name} <br>
+        / Телефон / ${body.phone} <br>
+        / Email / ${body.email} <br>
+        / Адрес объекта / ${body.info} <br>
+        / Комментарий / ${body.description} <br>
+      `,
     };
+
     const mailClient = {
-      form: `"${body.name}" <${config.CONTACTMAIL}>`,
+      from: `"Заявка с сайта" <${config.MAILUSER}>`,
       to: body.email,
       subject: "Заявка с сайта",
       text: body.info,
       html: body.description,
     };
+
     await transporter.sendMail(mail);
     //await transporter.sendMail(mailClient);
-  } catch (event) {
+  } catch (err) {
     sendError(
       event,
       createError({
         statusCode: 400,
-        statusMessage: error,
+        statusMessage: err.message,
       })
     );
   }
