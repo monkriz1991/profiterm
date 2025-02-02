88db9386961944eb1eb2d202a71de189a3e2d1e9
mynmind <mynmind@proton.me>
2025-02-02 12:49:59 +0300

    gtm

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup>
+useHead({
+  script: [
+    {
+      src: "https://www.googletagmanager.com/gtag/js?id=AW-815779075",
+      async: true,
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-815779075');
+      `,
+    },
+  ],
+});
+</script>
 <template>
   <div>
     <NuxtLayout>
